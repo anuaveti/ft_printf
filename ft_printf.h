9e6452010c578d04d241d72b1e671a6a1c94fd97
anuaveti <aaanuuuuushik@gmail.com>
2023-04-09 16:18:14 +0400

    initial header file

---
+++ ft_printf.h
@@ -0,0 +1,16 @@
+#ifndef FT_PRINT_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+# include "ft_printf_tp.h"
+# include "libft.h"
+
+# define __CONVERTER "cspdiuxX%"
+
+int	ft_printf(const char *, ...);
+
+#endif
